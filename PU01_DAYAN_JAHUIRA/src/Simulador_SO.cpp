fec34a24e5f9d5af010b2c501e7b1238655d4b4b
Dayan_JP <dayjahuira@upt.pe>
2025-09-17 11:53:23 -0500

    Declrando estructuras (strucstruct)

--- PU01_DAYAN_JAHUIRA/src/Simulador_SO.cpp
+++ PU01_DAYAN_JAHUIRA/src/Simulador_SO.cpp
@@ -3,6 +3,29 @@
 using json = nlohmann::json;
 using namespace std;
 
+struct Process {
+    int pid;
+    int llegada;
+    int servicio;
+    int inicio = -1;
+    int fin = -1;
+    int restante = 0;
+    // métricas calculables
+    int respuesta() const { return inicio - llegada; }
+    int espera() const { return fin - llegada - servicio; }
+    int retorno() const { return fin - llegada; }
+};
+
+struct MemReq { int pid; int tam; int block_id = -1; };
+
+struct Block {
+    int id;
+    int inicio;
+    int tam;
+    bool libre;
+    int pid_asignado; // -1 si libre
+};
+
 int main(int argc, char** argv){
 	ios::sync_with_stdio(false);
     cin.tie(nullptr);

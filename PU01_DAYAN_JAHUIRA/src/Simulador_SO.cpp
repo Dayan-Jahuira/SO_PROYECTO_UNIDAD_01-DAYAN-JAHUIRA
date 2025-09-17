418c15acc85921b3f3c6450f1c8b099dcf1ad6e9
Dayan_JP <dayjahuira@upt.pe>
2025-09-17 11:47:06 -0500

    Eliminar ejecutable del repo, para mantener solo código fuente

--- PU01_DAYAN_JAHUIRA/src/Simulador_SO.cpp
+++ PU01_DAYAN_JAHUIRA/src/Simulador_SO.cpp
@@ -3,7 +3,14 @@
 using json = nlohmann::json;
 using namespace std;
 
-int main(){
+int main(int argc, char** argv){
+	ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    string cfg = "../config/config.json";
+    if (argc >= 2) cfg = argv[1];
+    	cout<<"=============="<<endl;
+    	cout<<"== Correcto =="<<endl;
+    	cout<<"=============="<<endl;
 	
-	return 0;
+    return 0;
 }
